99ad83a6c2d6cd9de49621a108525fcb9b94e4b3
Muhammed k <105543029+muhammedk11@users.noreply.github.com>
2026-09-18 13:55:20 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (85.11%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+    
+};
+ haystack = "sadbutsad";
+ needle = "sad";
+ console.log(strStr(haystack,needle));
